3f78b95cda6bdc06697be3cecf17fcafc47d9fa7
Kallebe Sousa <kallebe87@gmail.com>
2021-01-15 23:19:58 -0300

    :construction: create homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Landing from '../pages/Landing/index';
+import Home from '../pages/Home/index';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
-    <Route path="/" exact component={Landing} />
+    <Route path="/" exact   component={Landing} />
+    <Route path="/homepage" component={Home} />
   </BrowserRouter>
 );
 
