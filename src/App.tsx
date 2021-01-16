47185c404f2cc5514355fc08e8a8522c2e8123bd
Kallebe Sousa <kallebe87@gmail.com>
2021-01-16 00:33:29 -0300

    :lipstick: global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Routes from './routes/index';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
   <>
     <Routes />
+    <GlobalStyle />
   </>
 );
 
